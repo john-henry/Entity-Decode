ec058c76141892dd1e3c0e94168a35d4dd4c4d52
john-henry <info@johnhenry.ie>
2016-02-27 01:23:55 +0000

    Update version number

--- entity_decode/addon.setup.php
+++ entity_decode/addon.setup.php
@@ -3,9 +3,9 @@
 return array(
       'author' => 'John Henry Donovan',
       'author_url' => 'http://johnhenry.ie',
-      'description' => 'This plugin converts all HTML entities to their applicable characters. It is a replacement for html_entity_decode()',
-      'docs_url' => '',
       'name' => 'Entity Decode',
+      'description' => 'This plugin converts all HTML entities to their applicable characters. It is a replacement for html_entity_decode()',
+      'version' => '1.0.0',
       'namespace' => 'JHD\EntityDecode',
-      'version' => '1.0'
+      'settings_exist' => FALSE
 );
