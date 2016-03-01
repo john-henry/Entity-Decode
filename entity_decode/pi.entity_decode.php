aa4504794f686e72df86a0eb2c3013f17bf4cb97
john-henry <info@johnhenry.ie>
2016-03-01 16:56:08 +0000

    Added property declarations

--- entity_decode/pi.entity_decode.php
+++ entity_decode/pi.entity_decode.php
@@ -1,16 +1,35 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+
 class Entity_decode {
 
-    var $return_data;
-    var $charset = 'UTF-8';
+    /*
+    * @var  string  The plugin return data
+    */
+    public $return_data
 
-    function __construct($str = '')
+    /*
+    * @var  string  Character set identifier
+    */
+    private $charset = 'UTF-8';
+
+    /**
+    * Constructor
+    *
+    * @access  public
+    * @return  void
+    */
+
+   public function __construct($str = '')
     {
         if(empty($str))
         {
-	        $str = ee()->TMPL->tagdata;
+          $str = ee()->TMPL->tagdata;
         }
         $this->return_data = ee('Security/XSS')->entity_decode($str, $charset);
     }
+    
+    // ------------------------------------------------------------------------
 }
+// END CLASS
+// EOF
